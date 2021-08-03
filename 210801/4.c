5c969b9f9525ddd87aeb32fa581f504aa7ceed0d
seobeomju <alt10063@naver.com>
2021-08-03 22:26:59 +0900

    백준 4번

---
+++ 210801/4.c
@@ -0,0 +1,13 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\ \n");
+	printf("|\"^\"`    | \n");
+	printf("||_/=\\\\__| ");
+	
+	return 0;
+}
