23a925ab64e9ace65841986c13199178dee26c91
seobeomju <alt10063@naver.com>
2021-08-03 23:09:13 +0900

    백준 8번

---
+++ part 1/8.c
@@ -0,0 +1,14 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+	return 0;
+}
