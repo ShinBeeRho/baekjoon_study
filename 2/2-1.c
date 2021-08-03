1dd63c85dbf91c4bca0bd867de2f39f5617d93c9
ShinBeeRho <nsbee7@naver.com>
2021-08-03 22:20:06 +0900

    210803

---
+++ 2/2-1.c
@@ -0,0 +1,11 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+int main(void) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a > b) printf(">");
+	else if (a < b) printf("<");
+	else if (a == b) printf("==");
+
+	return 0;
+}
