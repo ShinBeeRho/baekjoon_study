5ec267152e3fb8147d9aae21ea54a3b64e418f90
seobeomju <alt10063@naver.com>
2021-08-04 23:03:03 +0900

    백준 2-1번

---
+++ part 2/1.c
@@ -0,0 +1,15 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a < b)
+		printf("<");
+	else if (a > b)
+		printf(">");
+	else
+		printf("==");
+	return 0;
+}
