fd06c9f9521e407e7a9f8eb6d57aca6048344098
seobeomju <alt10063@naver.com>
2021-08-05 14:25:44 +0900

    백준 3-3

---
+++ part 3/3.c
@@ -0,0 +1,12 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+	int n,result=0;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		result += i;
+	printf("%d", result);
+	return 0;
+}
