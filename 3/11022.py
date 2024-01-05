51a73e3518040ffe44baf16d9c4c68c8172e5476
ShinBeeRho <88143264+ShinBeeRho@users.noreply.github.com>
2024-01-05 15:34:20 +0900

    Create 11022.py

---
+++ 3/11022.py
@@ -0,0 +1,10 @@
+#두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+import sys
+
+T = int(input())
+for i in range(T):
+  A, B = map(int, sys.stdin.readline().split())
+  print("Case #%d: %d + %d = %d"%((i + 1),A,B,(A + B)))
+
+#출력 양식에 주의하여 출력할 것
