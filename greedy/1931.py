143bfdaa3413bb5133dafac5a8a41d4951e5ee91
ShinBeeRho <nsbee7@naver.com>
2024-01-30 21:03:50 +0900

    240130

---
+++ greedy/1931.py
@@ -0,0 +1,11 @@
+INF = 2**31-1
+N=int(input())
+arr=[list(map(int,input().split())) for i in range(N)]
+arr.sort(key=lambda x:x[1])
+res=[[arr[0][0],arr[0][1]]]
+
+for i in range(1,N):
+    if arr[i][0] >= res[-1][1] :
+        res.append(arr[i])
+
+print(len(res))    
---
+++ greedy/practice4.py
@@ -0,0 +1,10 @@
+N,K=map(int,input().split())
+ans=0
+while(N!=1):
+    if N%K==0:
+        N/=K
+    else:
+        N-=1
+    ans+=1
+    
+print(ans)
